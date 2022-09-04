64d9c08c1075a2eb93911cfccdc0e9ebb9cc91ff
Tatsuya1117 <othe.world.pc@gmail.com>
2022-09-04 16:12:25 +0900

    Footerを追加

---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header>
+      <Link href="/" passHref>
+        <a className="flex justify-item-center">
+          <Image
+            alt={"logo"}
+            height={50}
+            src={"logo.png"}
+            width={50}
+          />
+        </a>
+      </Link>
+      <Link href="/" passHref>
+        <a className="text-4xl text-center flex justify-items-center">
+          <h1 className="pt-4">Manga List</h1>
+        </a>
+      </Link>
+    </header>
+  )
+};
