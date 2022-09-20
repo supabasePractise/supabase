9e85f07f7a691e994e8e91f3b0bd95921d928395
Tatsuya1117 <othe.world.pc@gmail.com>
2022-09-20 22:35:59 +0900

    AddTitleに追記

--- src/components/AddTitle.tsx
+++ src/components/AddTitle.tsx
@@ -62,7 +62,55 @@ export const AddTitle = (props: Props) => {
               className=""
               onClose={closeModal}
             >
-            テスト
+            <div className="min-h-screen px-4 text-center boder-2">
+                <span
+                  className="inline-block h-screen align-middle"
+                  aria-hidden="true"
+                >
+                  &#8203;
+                </span>
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform border border-gray-300 shadow-xl bg-gray-50 rounded-xl">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-2xl font-medium leading-6 text-center text-gray-900"
+                    >
+                      Add Title
+                    </Dialog.Title>
+                    <div className="grid grid-cols-4 gap-2 mt-4">
+                        <div className="col-span-1 text-xl text-center">Title</div>
+                        <input 
+                          className="w-full h-10 col-span-3 p-2 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700"
+                          value={title}
+                          onChange={(e) => {
+                            return setTitle(e.target.value);
+                          }}
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 gap-2 mt-4">
+                      <div className="col-span-1 text-xl text-center">Author</div>
+                      <input 
+                        className="w-full h-10 col-span-3 p-2 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700"
+                        value={author}
+                        onChange={(e) => {
+                            return setAuthor(e.target.value);
+                        }}
+                      />
+                    </div>
+                    <div>
+                        {/* ここからボタン */}
+                    </div>
+                  </div>
+                </Transition.Child>
+            </div>
             </Dialog>
           </Transition>
         </>
